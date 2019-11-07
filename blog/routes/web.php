a11b3565a49110fd07150c09495a190b789cd8ab
zafarshafaq <zafarshafaq34@gmail.com>
2019-11-07 11:16:49 +0430

    #3 Routes Defined

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -13,6 +13,47 @@
 
 Route::get('/', 'publicController@index');
 
+Route::get('/contact', 'publicController@contact');
+
+Route::get('/kacca-valuation-and-business-modeling', 'publicController@valuationBusinessModeling');
+
+Route::get('/kacca-transaction-tax', 'publicController@transactionTax');
+
+Route::get('/kacca-transaction-support', 'publicController@transactionSupport');
+
+Route::get('/kacca-environment-management-advisory', 'publicController@environmentManagementAdvisory');
+
+Route::get('/kacca-corporate-advisory', 'publicController@corporateAdvisory');
+
+Route::get('/kacca-advisory-services', 'publicController@advisoryServices');
+
+Route::get('/kacca-indirect-tax', 'publicController@indirectTax');
+
+Route::get('/kacca-transfer-pricing', 'publicController@transferPricing');
+
+Route::get('/kacca-international-tax', 'publicController@internationTax');
+
+Route::get('/kacca-business-receipt-tax', 'publicController@businessReceiptTax');
+
+Route::get('/kacca-business-tax-service', 'publicController@businessTaxService');
+
+Route::get('/kacca-tax-compilance', 'publicController@taxCompilance');
+
+Route::get('/kacca-management-assurance', 'publicController@managementAssurance');
+
+Route::get('/kacca-information-system-assurance', 'publicController@informationSystemAssurance');
+
+Route::get('/kacca-audit-accounting', 'publicController@auditAccounting');
+
+Route::get('/kacca-our-team', 'publicController@ourTeam');
+
+Route::get('/kacca-mission-vision', 'publicController@missionVision');
+
+Route::get('/kacca-history', 'publicController@history');
+
+Route::get('/kacca-history', 'publicController@history');
+
+
 
 
 
