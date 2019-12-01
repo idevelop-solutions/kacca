294cf53494a2dd577ddd6267b2cb9476ea888b7f
zafarshafaq <zafarshafaq34@gmail.com>
2019-12-01 16:03:26 +0430

    #9 Adding slogans to sliders

--- blog/resources/views/user/includes/slider.blade.php
+++ blog/resources/views/user/includes/slider.blade.php
@@ -1,48 +1,44 @@
 <section class="home-slider owl-carousel">
-      <div class="slider-item" style="background-image:url(images/bg_1.jpg);" data-stellar-background-ratio="0.5">
+      <div class="slider-item" style="background-image:url(images/b1.jpg);" data-stellar-background-ratio="0.5">
       	<div class="overlay"></div>
         <div class="container">
           <div class="row no-gutters slider-text align-items-center justify-content-end" data-scrollax-parent="true">
           <div class="col-md-6 text ftco-animate">
-            <!-- <h1 class="mb-4"><span>Stay Happy One</span></h1>
-            <h3 class="subheading">Everyday We Bring Hope and Smile to the Patient We Serve</h3>
-            <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">View our works</a></p> -->
+            <h1 class="mb-4"><span>Audit Service</span></h1>
+            <h3 class="subheading">insure your business sustainability with us</h3>
           </div>
         </div>
         </div>
       </div>
-	  <div class="slider-item" style="background-image:url(images/bg_2.jpg);" data-stellar-background-ratio="0.5">
+	  <div class="slider-item" style="background-image:url(images/b2.jpg);" data-stellar-background-ratio="0.5">
       	<div class="overlay"></div>
         <div class="container">
           <div class="row no-gutters slider-text align-items-center justify-content-end" data-scrollax-parent="true">
           <div class="col-md-6 text ftco-animate">
-            <!-- <h1 class="mb-4"><span>Stay Happy One</span></h1>
-            <h3 class="subheading">Everyday We Bring Hope and Smile to the Patient We Serve</h3>
-            <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">View our works</a></p> -->
+            <h1 class="mb-4"><span>Taxation service</span></h1>
+            <h3 class="subheading">Correct place to solve your tax problems</h3>
           </div>
         </div>
         </div>
       </div>
-	  <div class="slider-item" style="background-image:url(images/bg_3.jpg);" data-stellar-background-ratio="0.5">
+	  <div class="slider-item" style="background-image:url(images/b3.jpg);" data-stellar-background-ratio="0.5">
       	<div class="overlay"></div>
         <div class="container">
           <div class="row no-gutters slider-text align-items-center justify-content-end" data-scrollax-parent="true">
           <div class="col-md-6 text ftco-animate">
-            <!-- <h1 class="mb-4"><span>Stay Happy One</span></h1>
-            <h3 class="subheading">Everyday We Bring Hope and Smile to the Patient We Serve</h3>
-            <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">View our works</a></p> -->
+		  <h1 class="mb-4"><span>Assurance service</span></h1>
+            <h3 class="subheading">We care about your organization</h3>
           </div>
         </div>
         </div>
       </div>
-      <div class="slider-item" style="background-image:url(images/bg_4.jpg);">
+      <div class="slider-item" style="background-image:url(images/b4.jpg);">
       	<div class="overlay"></div>
         <div class="container">
           <div class="row no-gutters slider-text align-items-center justify-content-end" data-scrollax-parent="true">
           <div class="col-md-6 text ftco-animate">
-            <!-- <h1 class="mb-4">Smile Makes <br>A Lasting Impression</h1>
-            <h3 class="subheading">Your Health is Our Top Priority with Comprehensive, Affordable medical.</h3>
-            <p><a href="#" class="btn btn-secondary px-4 py-3 mt-3">View our works</a></p> -->
+		  <h1 class="mb-4"><span>Advisory service</span></h1>
+            <h3 class="subheading">Trust us, your business can grow with us</h3>
           </div>
         </div>
         </div>
